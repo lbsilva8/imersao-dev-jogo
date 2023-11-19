06c566622aaf31c9c0200856503363395c66ef44
Lorena <lorenaborgessilva@gmail.com>
2023-11-19 01:53:02 -0300

    adicionando tentativas

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 var numeroSecreto = parseInt(Math.random() * 11);
+var tentativas = 0
+var tentativasMax = 5
 
-while(chute != numeroSecreto){
-    var chute = prompt('Digite um numero de 0 a 10')
+while(chute != numeroSecreto & tentativas < tentativasMax){
+    var chute = prompt('Digite um numero de 0 a 10, você tem 5 tentativas')
+    tentativas++
     if(chute == numeroSecreto){
-        alert('Isso ai, você descobriu o numero secreto!')
+        alert('Isso ai, você descobriu o numero secreto! Você precisou de ' + tentativas + ' tentativas')
     }else if(chute > numeroSecreto){
-        alert('Ops! esse não é o numero,o numero secreto é menor. tente novamente.')
+        alert('Ops! esse não é o numero. O numero secreto é menor que ' + chute + '. tente novamente.')
     } else {
-        alert('Ops! esse não é o numero,o numero secreto é maior. tente novamente.')
+        alert('Ops! esse não é o numero. O numero secreto é maior que ' + chute +'. tente novamente.')
     }
 }
+alert('Suas tentativas acabaram, o numero secreto é: ' + numeroSecreto)
